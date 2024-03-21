687b2b6b7f71acbe6710967555d3f0a59cb9e02a
Vitalii Lobanov <lobanov@bootandpencil.com>
2024-03-21 13:03:46 +0400

    Colored logger, module names fixed

--- app/utils/debug.py
+++ app/utils/debug.py
@@ -81,6 +81,71 @@
 # logger.error("An error message")
 # logger.critical("A critical message")
 
+
+#------------------------------------
+
+# import logging
+# import colorlog
+
+# class CustomColorLogger(logging.Logger):
+#     def __init__(self, name, level=logging.NOTSET):
+#         super().__init__(name, level)
+# # %(asctime)s - %(levelname)s - [%(name)s] %(module)s.%(funcName)s: %(message)s
+#         colored_formatter = colorlog.ColoredFormatter(
+#             #"%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s %(blue)s%(asctime)s | %(module)s %(funcName)s",
+#              "%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s "
+#             "%(green)s%(asctime)s | %(module)s.%(funcName)s",
+#             datefmt=None,
+#             reset=True,
+#             log_colors={
+#                 'DEBUG': 'cyan',
+#                 'INFO': 'green',
+#                 'WARNING': 'yellow',
+#                 'ERROR': 'red',
+#                 'CRITICAL': 'red,bg_white',
+#             },
+#             secondary_log_colors={},
+#             style='%'
+#         )
+
+#         stream_handler = logging.StreamHandler()
+#         stream_handler.setFormatter(colored_formatter)
+#         self.addHandler(stream_handler)
+
+#     def debug(self, msg, *args, **kwargs):
+#         # Custom implementation for debug
+#         super().debug(msg, *args, **kwargs)
+
+#     def info(self, msg, *args, **kwargs):
+#         # Custom implementation for info
+#         super().info(msg, *args, **kwargs)
+
+#     def warning(self, msg, *args, **kwargs):
+#         # Custom implementation for warning
+#         super().warning(msg, *args, **kwargs)
+
+#     def error(self, msg, *args, **kwargs):
+#         # Custom implementation for error
+#         super().error(msg, *args, **kwargs)
+
+#     def critical(self, msg, *args, **kwargs):
+#         # Custom implementation for critical
+#         super().critical(msg, *args, **kwargs)
+
+# def test_func():
+#     logger.debug("This is a debug message")
+#     logger.info("This is an info message")
+#     logger.warning("This is a warning message")
+#     logger.error("This is an error message")
+#     logger.critical("This is a critical message")
+
+# # Example usage
+# logger = CustomColorLogger('custom_logger')
+
+# test_func()
+
+#------------------------------------
+
 import logging
 import colorlog
 
@@ -88,7 +153,8 @@ class CustomColorLogger(logging.Logger):
     def __init__(self, name, level=logging.NOTSET):
         super().__init__(name, level)
         colored_formatter = colorlog.ColoredFormatter(
-            "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+            "%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)-90s "
+            "%(white)s%(bg_blue)s%(asctime)s | %(module)s.%(funcName)s",
             datefmt=None,
             reset=True,
             log_colors={
@@ -107,30 +173,33 @@ class CustomColorLogger(logging.Logger):
         self.addHandler(stream_handler)
 
     def debug(self, msg, *args, **kwargs):
-        # Custom implementation for debug
-        super().debug(msg, *args, **kwargs)
+        # Custom additional handling for debug
+        self._log(logging.DEBUG, msg, args, exc_info=None, extra=None, stack_info=False, stacklevel=2, **kwargs)
 
     def info(self, msg, *args, **kwargs):
-        # Custom implementation for info
-        super().info(msg, *args, **kwargs)
+        # Custom additional handling for info
+        self._log(logging.INFO, msg, args, exc_info=None, extra=None, stack_info=False, stacklevel=2, **kwargs)
 
     def warning(self, msg, *args, **kwargs):
-        # Custom implementation for warning
-        super().warning(msg, *args, **kwargs)
+        # Custom additional handling for warning
+        self._log(logging.WARNING, msg, args, exc_info=None, extra=None, stack_info=False, stacklevel=2, **kwargs)
 
     def error(self, msg, *args, **kwargs):
-        # Custom implementation for error
-        super().error(msg, *args, **kwargs)
+        # Custom additional handling for error
+        self._log(logging.ERROR, msg, args, exc_info=None, extra=None, stack_info=False, stacklevel=2, **kwargs)
 
     def critical(self, msg, *args, **kwargs):
-        # Custom implementation for critical
-        super().critical(msg, *args, **kwargs)
+        # Custom additional handling for critical
+        self._log(logging.CRITICAL, msg, args, exc_info=None, extra=None, stack_info=False, stacklevel=2, **kwargs)
 
 # Example usage
 logger = CustomColorLogger('custom_logger')
 
-logger.debug("This is a debug message")
-logger.info("This is an info message")
-logger.warning("This is a warning message")
-logger.error("This is an error message")
-logger.critical("This is a critical message")
+def example_function():
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
+
+example_function()
