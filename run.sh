9ad1d188e77338b86576dc6f8fc798872a2c78d3
Vitalii Lobanov <lobanov@bootandpencil.com>
2024-04-11 09:08:04 +0400

    Shell script + reqs

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Change to the script's directory
+cd "$SCRIPT_DIR"
+
+# Set environment variables
+export TELEGRAM_API_KEY="6910899160:AAEABnf-OSCV3mA04uNYeYRcJ9_PD69ufZ0"
+export PYTHONPATH="$(pwd)/app"
+export LOG_LEVEL="DEBUG"
+export FORWARD_DEBUG_MESSAGES_TO_USERS="TRUE"
+
+# Get the full path to the current working directory
+WORKING_DIR=$(pwd)
+
+# Start the Python debugger with the current file
+# Replace 'your_script.py' with the script you want to debug
+python  "$WORKING_DIR/app/main.py" > bot.log 2>&1
